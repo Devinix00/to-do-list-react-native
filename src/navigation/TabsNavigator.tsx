223b3e806fb5ed1ee92b78ea567599d28f641256
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-11-26 22:45:57 +0900

    components 디렉토리 수정

--- src/navigation/TabsNavigator.tsx
+++ src/navigation/TabsNavigator.tsx
@@ -1,8 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 
-import { HapticTab } from "@/components/haptic-tab";
-import { IconSymbol } from "@/components/ui/icon-symbol";
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/IconSymbol";
 import { Colors } from "@/constants/theme";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { TabsParamList } from "@/src/navigation/types";
