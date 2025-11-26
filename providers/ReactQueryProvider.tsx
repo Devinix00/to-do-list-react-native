ee2ed8412f18c1b283caac939e63170b770deec9
김범수 <121074744+Devinix00@users.noreply.github.com>
2025-11-26 21:03:59 +0900

    react query provider설정

---
+++ providers/ReactQueryProvider.tsx
@@ -0,0 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+
+const queryClient = new QueryClient();
+
+function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
+
+export default ReactQueryProvider;
